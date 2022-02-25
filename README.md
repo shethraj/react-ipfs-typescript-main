b8f16f22fc1152f45a1da5e7296acd9a866f8fb7
RaJ-ShEtH-2807 <79966397+RaJ-ShEtH-2807@users.noreply.github.com>
2022-02-25 16:11:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Example of React + TypeScript using IPFS to upload files
+
+This is a simple React TypeScript app to show how to upload files using IPFS Infura.
+
+If you are cloning this project, make sure to update the `projectId` and `projectSecret` in the App.tsx file.
---
+++ src/App.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import "./App.css";
+import { create, CID, IPFSHTTPClient } from "ipfs-http-client"; // used for importing IPFS CLIENT
+
+const projectId = '25UsGKMpbjSU9F2ByN4f46sU4D7';
+const projectSecret = 'a9ab0dc18e02ae5ece21af996704ce70';
+const authorization = "Basic " + btoa(projectId + ":" + projectSecret); //it generates auth key for accesing IPFS
+
+function App() {
+  //main react App
+  const [images, setImages] = React.useState<{ cid: CID; path: string }[]>([]);
+
+  let ipfs: IPFSHTTPClient | undefined;
+  try {
+    ipfs = create({
+      url: "https://ipfs.infura.io:5001/api/v0",
+      headers: {
+        authorization,
+      },
+    });
+  } catch (error) {
+    console.error("IPFS error ", error);
+    ipfs = undefined;
+  }
+
+  /**
+   * @description event handler that uploads the file selected by the user
+   */
+  const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const files = (form[0] as HTMLInputElement).files;
+
+    if (!files || files.length === 0) {
+      return alert("No files selected");
+    }
+
+    const file = files[0];
+    // upload files
+    const result = await (ipfs as IPFSHTTPClient).add(file);
+
+    const uniquePaths = new Set([
+      ...images.map((image) => image.path),
+      result.path,
+    ]);
+    const uniqueImages = [...uniquePaths.values()]
+      .map((path) => {
+        return [
+          ...images,
+          {
+            cid: result.cid,
+            path: result.path,
+          },
+        ].find((image) => image.path === path);
+      });
+    
+      // @ts-ignore
+    setImages(uniqueImages);
+
+    form.reset();
+  };
+
+  console.log("images ", images);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {ipfs && (
+          <>
+            <p>CREATE NFT</p>
+            
+            <form onSubmit={onSubmitHandler}>
+              <input name="file" type="file" />
+
+              <button type="submit">Upload File</button>
+            </form>
+
+            <div>
+              {images.map((image, index) => (
+                <img
+                  alt={`Uploaded #${index + 1}`}
+                  src={"https://ipfs.infura.io/ipfs/" + image.path}
+                  style={{ maxWidth: "400px", margin: "15px" }}
+                  key={image.cid.toString() + index}
+                />
+              ))}
+            </div>
+          </>
+        )}
+
+        {!ipfs && (
+          <p>Oh oh, Not connected to IPFS. Checkout out the logs for errors</p>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
